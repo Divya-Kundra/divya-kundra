aca27ee29e9b15354f24f4053e2fdfcdfe87f924
Divya Kundra <divyakundra21@gmail.com>
2023-01-10 17:51:09 -0500

    Remove framework text

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,17 +1,21 @@
 <template>
   <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+    <div class="column flex-center">
+      <div class="text-h1 text-primary">Divya Kundra</div>
+      <div class="text-h4 text-secondary">FrontEnd Developer</div>
+      <!-- <img
+        alt="Quasar logo"
+        src="~assets/quasar-logo-vertical.svg"
+        style="width: 200px; height: 200px"
+      /> -->
+    </div>
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: 'IndexPage'
-})
+  name: "IndexPage",
+});
 </script>
